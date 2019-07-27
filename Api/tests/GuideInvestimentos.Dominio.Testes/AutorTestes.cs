040c5769a89c8fee7d1d2520a4e4993dda55bca7
Cleber Varçal <cleber.varcal@outlook.com>
2019-07-27 13:55:34 -0300

    Configurado projeto

---
+++ Api/tests/GuideInvestimentos.Dominio.Testes/AutorTestes.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace GuideInvestimentos.Dominio.Testes
+{
+    public class AutorTestes
+    {
+        [Fact(DisplayName = "Formar nome do autor")]
+        [Trait("Dominio","Autor")]
+        public void Formatar_nome_do_autor()
+        {
+            
+        }
+    }
+}
